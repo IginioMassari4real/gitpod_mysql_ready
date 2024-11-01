8bdbbe96b00d8317c96ae21114be94f31434f967
Tommaso Rossi <rossi.tommaso@iisgalvanimi.edu.it>
2024-11-01 12:59:57 +0000

    punto B finito

---
+++ prep_B.py
@@ -0,0 +1,26 @@
+import mysql.connector
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  password=""
+)
+
+mycursor = mydb.cursor()
+
+sql = "INSERT INTO Mammiferi (id INT AUTO_INCREMENT PRIMARY KEY, nome_proprio VARCHAR(255), razza VARCHAR(255), peso INT, eta INT) VALUES (%s, %s)"
+val = [
+  ('Leone', 'mammifero', '200', '30'),
+  ('Balena', 'mammifero', '1000', '25'),
+  ('Delfino', 'mammifero', '200', '35'),
+  ('Elefante', 'mammifero', '900', '40'),
+  ('Lupo', 'mammifero', '150', '20'),
+]
+
+mycursor.executemany(sql, val)
+
+mydb.commit()
+
+print(mycursor.rowcount, "was inserted.")
+
+#Viene utilizzata la funzione executemany per inserire più record contemporaneamente nella tabella customers. Dopo l'inserimento, commit() salva le modifiche e stampa il numero di record inseriti.
